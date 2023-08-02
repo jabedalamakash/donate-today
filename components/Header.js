178d68165c82920724ba63cfc161c9c61d731efc
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-02 21:59:40 +0600

    bug fixed

--- components/Header.js
+++ components/Header.js
@@ -20,11 +20,9 @@ const Header = () => {
                 />
                 </div>
                 <div className=' relative text-center sm:min-h-screen -tracking-tighter sm:grid place-content-center z-10  mx-auto sm:py-20 sm:px-20 sm:p-4  text-white '>
-                <h1 className='mb-1 sm:mb-4 sm:px-20 sm:mx-[320px] text-2xl sm:text-7xl text-center  font-bold'>Let's Help Those Who are
-                  
-                  More in Need
+                <h1 className=' mb-2 sm:mb-4 sm:px-20 sm:w-[900px] text-2xl sm:text-7xl text-center  font-bold'>Let's Help Those Who are More in Need
                 </h1>
-                <p className=' sm:block  sm:px-20 text-center sm:mb-4 sm:mx-[330px] text-sm sm:text-xl sm:text-thin'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet atque voluptate fuga voluptas! Amet iste obcaecati itaque nemo dolorum facilis.</p>
+                <p className=' mb-2 sm:mb-4   sm:px-20 text-center sm:w-[900px] text-xl sm:text-2xl sm:text-thin'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet atque voluptate fuga voluptas! Amet iste obcaecati itaque nemo dolorum facilis.</p>
                 <Btn title="Donate Now"/>
 
                 </div>
