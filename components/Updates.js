67ec81ab87439882d65eb34da471ab5a63d0eb9b
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-15 20:24:42 +0600

    fixede

--- components/Updates.js
+++ components/Updates.js
@@ -3,7 +3,7 @@ import Btn from './Btn';
 
 const Updates = () => {
     return (
-        <div className='w-screen sm:w-[80vh] mx-auto '>
+        <div className='w-full sm:w-[50%] mx-auto '>
                     <div className='text-center '>
                         <h1 className=' text-5xl mb-5 font-bold'>Get More Updates</h1>
                         <p className='text-xl px-20 font-thin mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestiae quasi iusto possimus? Deserunt id nostrum et aperiam magnam. Ducimus.</p>
